1541acc151c6045c2e89e13c4d27cb471b8dea59
Muhammad Waqar <muhammadwaqar.cp@gmail.com>
2021-08-11 10:52:11 +0500

    vehicle registration changes

--- backend/src/controllers/general.controller.js
+++ backend/src/controllers/general.controller.js
@@ -2,6 +2,7 @@ const { saveVehicle } = require("../services/vehicle.service");
 const httpStatus = require("http-status");
 const AUX = require("../helpers/auxilaries");
 const EVENT = require("../triggers/custom-events").customEvent;
+const { Vehicle } = require("../models");
 
 const test = function (req, files, res) {
   res.status(200).send({
@@ -12,7 +13,14 @@ const test = function (req, files, res) {
 const vehicleRegistration = async (params, files, res) => {
   try {
     const vehicle = await saveVehicle(params);
-    await AUX.uploadToAws(files[0].buffer, vehicle._id, files[0].mimetype);
+    const photo = await AUX.uploadToAws(
+      files[0].buffer,
+      vehicle._id,
+      files[0].mimetype
+    );
+
+    await Vehicle.findByIdAndUpdate(vehicle._id, { photo: photo.Location });
+
     EVENT.emit("update-vehicle-in-user", vehicle._id, params.userId);
     res.status(httpStatus.OK).send({
       status: true,
